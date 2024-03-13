1604c0bacbadfafd44300cdb9d8992b7f4c6f39a
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-03-13 03:53:48 -0500

    install json server to bring in data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
+
+  useEffect(function() {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error"));
+  }, [])
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   )
 }
